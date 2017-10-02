d8fe6e3946ea8341e7c1d441df758280faeb484c
murnat98 <murnat98@yandex.ru>
2017-10-02 13:34:20 +0300

    core

--- project/application/settings.py
+++ project/application/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -78,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'test',
         'USER': 'root',
-        'PASSWORD': 'muradik98',
+        'PASSWORD': 'password',
         'HOST': 'localhost',
     }
 }
