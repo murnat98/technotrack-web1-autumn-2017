09daf4fe5f7a2ac51444c0a960e4872263b41799
murnat98 <murnat98@yandex.ru>
2017-11-15 16:10:11 +0300

    adding answers

--- project/question/forms.py
+++ project/question/forms.py
@@ -1,10 +1,16 @@
 from django import forms
 from django.http import request
 
-from question.models import Questions
+from question.models import Questions, Answers
 
 
 class CreateQuestionForm(forms.ModelForm):
     class Meta:
         model = Questions
         fields = ['category', 'title', 'text']
+
+
+class CreateAnswerForm(forms.ModelForm):
+    class Meta:
+        model = Answers
+        fields = ['title', 'text']
--- project/question/views.py
+++ project/question/views.py
@@ -2,12 +2,13 @@
 from __future__ import unicode_literals
 
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import get_object_or_404
 from django.urls import reverse
 from django.views.generic import ListView, DetailView, CreateView
 
 from application import settings
-from question.forms import CreateQuestionForm
-from question.models import Questions, Categories
+from question.forms import CreateQuestionForm, CreateAnswerForm
+from question.models import Questions, Categories, Answers
 
 
 class CategoriesList(ListView):
@@ -28,10 +29,35 @@ class QuestionList(ListView):
     context_object_name = 'questions'
 
 
-class QuestionDetail(DetailView):
-    model = Questions
+class QuestionDetail(CreateView):
+    model = Answers
     template_name = 'question/question_detail.html'
-    context_object_name = 'question'
+    form_class = CreateAnswerForm
+
+    def __init__(self, **kwargs):
+        super(QuestionDetail, self).__init__(**kwargs)
+        self.question = None
+
+    def get_success_url(self):
+        return reverse('questions:question_detail', kwargs={'pk': self.kwargs['pk']})
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        form.instance.question = get_object_or_404(Questions, id=self.kwargs['pk'])
+
+        return super(QuestionDetail, self).form_valid(form)
+
+    def dispatch(self, request, pk=None, *args, **kwargs):
+        self.question = get_object_or_404(Questions, id=pk)
+
+        return super(QuestionDetail, self).dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(QuestionDetail, self).get_context_data(**kwargs)
+
+        context['question'] = self.question
+
+        return context
 
 
 class CreateQuestionView(LoginRequiredMixin, CreateView):
